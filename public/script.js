60a5f0b2186a719630cec45e57719e10f888e3fe
the-cryptic-coder <divyambanga@gmail.com>
2024-12-12 01:22:32 -0500

    slow

--- public/script.js
+++ public/script.js
@@ -154,10 +154,10 @@ function drawGame(absorbingParticleX = null, absorbingParticleY = null) {
 
 function gameLoop() {
     drawGame();
-    if (keys.ArrowUp) socket.emit('move', { dx: 0, dy: -5 });
-    if (keys.ArrowDown) socket.emit('move', { dx: 0, dy: 5 });
-    if (keys.ArrowLeft) socket.emit('move', { dx: -5, dy: 0 });
-    if (keys.ArrowRight) socket.emit('move', { dx: 5, dy: 0 });
+    if (keys.ArrowUp) socket.emit('move', { dx: 0, dy: -2 });
+    if (keys.ArrowDown) socket.emit('move', { dx: 0, dy: 2 });
+    if (keys.ArrowLeft) socket.emit('move', { dx: -2, dy: 0 });
+    if (keys.ArrowRight) socket.emit('move', { dx: 2, dy: 0 });
     requestAnimationFrame(gameLoop);
 }
 
